4e8db7ab3d11903016638ac9b0a59f0fb0be8096
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2022-10-17 15:51:28 +1300

    Add JSON exceptions, add formatting config

--- web_interface/__main__.py
+++ web_interface/__main__.py
@@ -1,8 +1,13 @@
 import logging
+from datetime import datetime
+from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from rich.logging import RichHandler
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from common import __version__, get_project_root
 from common.console import CONSOLE
@@ -41,6 +46,35 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
-app.mount(
-    "/static", StaticFiles(directory=get_project_root() / "static"), name="static"
-)
+app.mount("/static", StaticFiles(directory=get_project_root() / "static"), name="static")
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    status = HTTPStatus(exc.status_code)
+    return JSONResponse(
+        status_code=status.value,
+        content={
+            "timestamp": datetime.now().replace(microsecond=0).isoformat(),
+            "status": f"{status.value}: {status.phrase}",
+            "details": [exc.detail],
+        },
+        headers=exc.headers,
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    status = HTTPStatus(422)
+    details = []
+    for error in exc.errors():
+        temp = ".".join(error["loc"])
+        details.append(f"{temp}: {error['msg']}")
+    return JSONResponse(
+        status_code=status.value,
+        content={
+            "timestamp": datetime.now().replace(microsecond=0).isoformat(),
+            "status": f"{status.value}: {status.phrase}",
+            "details": details,
+        },
+    )
